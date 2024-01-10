bc6240e8c5c9a548b41c8cc952aead0b7e393002
rohith <rohithappuh@gmail.com>
2024-01-10 17:06:11 +0530

    added model

--- app/models/product-model.js
+++ app/models/product-model.js
@@ -3,9 +3,52 @@ const mongoose = require('mongoose')
 const{ Schema, model} = mongoose
 
 const ProductSchema = new Schema ({
+    name : String,
+    categories : [{
+        categoryId : {
+            type : Schema.Types.ObjectId,
+            ref : "Category"
+        } 
+    }],
+    description : String,
+    price : Number,
+    stock : Number,
+    minStock : Number,
+    images : [String]
 
 })
 
 const Product = model('product', ProductSchema)
 
 module.exports = Product
+
+// name
+// categoryId 
+// description
+// price
+// unit
+// stock 
+// minStock 
+// images
+
+// # Order
+// customerId  
+// products - [ { productId, quantity, price } ]
+// total
+// status - [] 
+// addressId 
+// agentId 
+
+// # Cart
+// customerId  
+// products - [
+//     { productId, quantity }
+// ]
+
+// # Payment 
+// userId
+// orderId
+// amount
+// status
+// paymentType
+// transactionId 
