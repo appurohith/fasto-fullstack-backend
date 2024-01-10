41c4aeebb044a5434cc3870b446f98e71907ec94
rohith <rohithappuh@gmail.com>
2024-01-10 12:38:07 +0530

    new file

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const cors = require('cors')
 const PORT = 3040
 const app = express()
 app.use(express.json())
+const {checkSchema } = require('express-validator')
 
 const configureDB = require('./config/db')
 configureDB()
 
+const usersCltr = require('./app/controllers/users-cltr')
 
+const {registerSchema } = require('./app/validations/user-validation')
+
+
+//user APIS
+
+app.post('/api/user/register',checkSchema(registerSchema), usersCltr.register)
 
 
 app.listen(PORT, () => {
