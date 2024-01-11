59e8998e2133e644362ca8ffe4763f14a0180e86
sujithmouli <suchith.m.gadde45@gmail.com>
2024-01-11 17:51:47 +0530

    order-model

--- app/models/order-model.js
+++ app/models/order-model.js
@@ -1,3 +1,29 @@
 const mongoose = require('mongoose')
 
 const{ Schema, model} = mongoose
+
+const orderSchema = new Schema({
+    customerId : {
+        type: Schema.Types.ObjectId,
+        ref : 'Customer'
+        
+    },
+    products:[
+        {
+            productsId : {
+                type: Schema.Types.ObjectId,
+                ref: 'Product'
+            },
+            quantity: String,
+            price:String
+        }
+    ],
+    total: Number,
+    status:[
+        {
+            type: string
+        }
+    ],
+    
+
+})
