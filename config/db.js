fcdd3a877b848229bafbbaa57f00492b521a3fbf
moulisujith45 <suchith.m.gadde45@gmail.com>
2024-01-26 12:40:06 +0530

    created usersprofile

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const configureDB  = async () => {
     try {
-        const db = await mongoose.connect('mongodb://127.0.0.1:27017/fasto-app')
+        const db = await mongoose.connect('mongodb+srv://suchithmgadde45:suchith45@fasto1.uhqpluz.mongodb.net/?retryWrites=true&w=majority')
         console.log("successfully connected to db")
     }
     catch(e){
