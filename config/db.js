18867162ec9dcf7125c3584fb1a1964bc883ad9d
moulisujith45 <suchith.m.gadde45@gmail.com>
2024-01-26 16:28:31 +0530

    sujith

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const configureDB  = async () => {
     try {
-        const db = await mongoose.connect('mongodb+srv://suchithmgadde45:suchith45@fasto1.uhqpluz.mongodb.net/?retryWrites=true&w=majority')
+        const db = await mongoose.connect('mongodb+srv://suchithmgadde45:suchith45@fasto1.uhqpluz.mongodb.net/fasto1?retryWrites=true&w=majority')
         console.log("successfully connected to db")
     }
     catch(e){
