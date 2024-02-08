76eddaba4f54373ba57e4e8658a4c7e26a50b221
rohith <rohithappuh@gmail.com>
2024-02-08 15:39:16 +0530

    added stripe

--- app/controllers/payment-cltr.js
+++ app/controllers/payment-cltr.js
@@ -1,15 +1,125 @@
-const { valodationResult, validationResult } = require('express-validator')
-const Payment = require('../models/payment-model')
-const_ = require('lodash')
+require("dotenv").config()
+const { validationResult } = require("express-validator");
+const Order = require('../models/order-model')
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+const _ = require("lodash");
+const PaymentModel = require("../models/payment-model")
+const User = require("../models/user-model")
+const Cart = require('../models/cart-model')
+const paymentCltr = {};
 
-const paymentsCltr = {}
-
-paymentsCltr.create = async (req,res) => {
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array()})
-    } 
-    const body = _.pick(req.body,['amount','orderId'])
+paymentCltr.paymentCheckoutSession = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ error: errors.array() });
+  } else {
+    const body = _.pick(req.body, ['amount','orderId'])
     
 
-}
+    try {
+      const user = await User.findById(req.user.id)
+    //   console.log(user.username)
+    //   const order = await Order.findOne({ _id:body.orderId, customerId:req.user.id}).populate('cart.cartId')
+    // //   console.log(order)
+    // //   return res.json(order)
+    //      // Populate the 'products' field in each cart
+    //      for (let cartItem of order.cart) {
+    //         await Cart.populate(cartItem, {
+    //             path: 'cartId.products.productId',
+    //             model: 'Product',
+    //             select: 'name image' 
+    //         });
+            
+    //     }
+
+    //     return res.json(order)
+ 
+   
+    const order = await Order.findById(body.orderId).populate({
+        path: 'cart',
+        populate: {
+            path: 'products.productId',
+            model: 'Product',
+            select: 'name price image quantity'
+        }
+    });
+    
+    return res.json(order);
+    
+      
+    const customer = await stripe.customers.create({
+      // name: profile.userId.username,
+      name:"FASTO",
+      address: {
+          line1: 'India',
+          postal_code: '560002',
+          city: 'Banglore',
+          state: 'KA',
+          country: 'US', 
+      },
+  })
+
+  const cartitems = order.cart.cartId.products.map((ele) => {
+    return {
+      price_data: {
+        currency: "inr",
+        product_data: {
+          name: ele.productId.name,
+        //   images : [`https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.in%2FFresh-Potato-1kg-Pack%2Fdp%2FB07BG5GZP2&psig=AOvVaw1a_DbkXw3cTR7R3z0eTzAi&ust=1707466559690000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCODWit-mm4QDFQAAAAAdAAAAABAE`]
+        //   Images : [`https://fantasy11.s3.ap-south-1.amazonaws.com/players/Virat%20Kohli.png`]
+        },
+        unit_amount: ele.price * 100, // not done the converting to cents for usd
+      },
+      quantity: ele.quantity,
+    };
+  })
+      
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        mode: "payment",
+        line_items: cartitems,
+        customer:customer.id,
+
+        success_url: `${process.env.FRONTEND_URL}/success`,
+        cancel_url: `${process.env.FRONTEND_URL}/cancel`,
+      }); 
+      console.log(session,"in srssion")
+
+        res.json({id:session.id,url:session.url})
+
+        if(session.id){
+          const paymentPending = new PaymentModel({
+            customerId:req.user.id,
+            orderId:body.orderId,
+            paymentDate:new Date(),
+            amount :body.amount,
+            paymentType:session.payment_method_types[0],
+            transaction_Id:session.id
+        })
+      await paymentPending.save()
+        
+      }
+
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Internal server error" })
+    }
+  }
+};
+
+
+
+
+// paymentCltr.deletePayment  = async(req,res)=>{
+//   const {paymentId} = req.params
+//   try{
+//     await PaymentModel.findOneAndDelete({userId:req.user.id,transaction_Id:paymentId})
+//     return res.status(200).json("Somthing went wrong on the payment")
+//   }catch(err){//write the status code for payments
+//     return res.json(EvalError)
+//   }
+// }
+
+
+
+module.exports = paymentCltr;
--- app/models/payment-model.js
+++ app/models/payment-model.js
@@ -1,27 +1,30 @@
-const mongoose = require('mongoose')
-
-const{ Schema, model} = mongoose
+const { Schema, model } = require("mongoose");
 
 const paymentSchema = new Schema({
-    customerId : {
-        type:Schema.Types.ObjectId,
-        ref : 'User'
+    customerId: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
     },
-    amount : Number,
-    date : String,
-    status : {
-        type : String,
-        default : "pending",
-        enum : ["pending","successful"]
+    orderId: {
+        type: Schema.Types.ObjectId,
+        ref: "Order"
     },
-    paymentType : String,
-    transaction_Id : String
-})
+    paymentDate: Date,
+    amount: Number,
+    paymentType: String,
+    transaction_Id: String,
+    status: {
+        type: Boolean,   // Payment status
+        default: false
+    }
+});
+
+const PaymentModel = model("PaymentModel", paymentSchema);
+
+module.exports = PaymentModel;
 
 
-const Payment = model("Payment",paymentSchema)
 
-module.exports = Payment
 // # Payment 
 // userId
 // orderId
@@ -29,3 +32,54 @@ module.exports = Payment
 // status
 // paymentType
 // transactionId 
+
+// paymentCltr.create = async (req,res) => {
+//     const body = _.pick(req.body,['products','totalAmount','userEmail'])    
+//     try {
+//         const session = await stripe.checkout.sessions.create({
+
+//             payment_method_types : ['card'],
+//             mode : 'payment',
+//             line_items : body.products.map(product => {
+//                 return {
+//                     price_data : {
+//                         currency : 'inr',
+//                         product_data : {
+//                             name : product.title
+//                         },
+//                         unit_amount : product.price * 100
+//                     },
+//                     quantity : product.quantity
+//                 }
+//             }),
+//             success_url: "http://localhost:3000/myCart?success=true",
+//             cancel_url: "http://localhost:3000/myCart?cancel=true",
+//         })
+//         const payment = new Payment(body)
+//         payment.user = req.user.id,
+//         payment.transactionId = session.id
+//         payment.userEmail = body.userEmail
+//         payment.totalAmount = body.totalAmount
+        
+//         await payment.save()
+//         //console.log('pay',payment)
+
+//         // if(body.userEmail){
+//         //     const info = await transporter.sendMail({
+//         //         from: `Shrikant Shivangi ${process.env.NODE_MAILER_MAIL}`, // sender address
+//         //         to: `${body.userEmail}`, // list of receivers
+//         //         subject: "Order Recieved", // Subject line
+//         //         text: "We have recieved your order.Thanks! for being a part of reStock.", // plain text body
+//         //         html: "<b>We have recieved your order.Thanks! for being a part of reStock.</b>", // html body
+//         //       });
+        
+//         //       //console.log("Message sent: %s", info.messageId);
+//         // }
+//         res.json({
+//             url :session.url,
+//             id : session.id
+//         })
+//     } catch (e) {
+//         console.log('pc',e)
+//     }
+// }
